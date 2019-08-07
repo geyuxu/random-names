1d13c47a15900501bf91a70863ba580663caf51a
葛于旭 <yuxu.ge@bl.com>
2019-08-07 14:11:01 +0800

    华丽版

--- run.js
+++ run.js
@@ -1,11 +1,25 @@
+var names;
+var interval;
+
 $(document).ready(function(){
     $("#btn").click(doRandom); 
 });
 
 doRandom = function(){
-    var names = $("#names").val().split(",");
-    $(".dd")
-        .html(names[randomNum(0,names.length-1)]);
+    if($("#btn").html() == '开始'){
+        names = $("#names").val().split(",");
+        show();
+        $("#btn").html("停")
+    }else{
+        clearInterval(interval);
+        $("#btn").html("开始")
+    }
+}
+
+show = function(){
+    interval = setInterval(function(){
+        $(".dd").html(names[randomNum(0,names.length-1)]);
+    },35);  
 }
 
 randomNum = function (minNum,maxNum){ 
